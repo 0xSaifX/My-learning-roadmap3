0dde1f0b3e471e2ea77e40c6d573b3d66264c981
SaifX <saifuddeen.tech@gmail.com>
2025-11-19 12:39:57 +0100

    Refactor HomeArtists to HomeScreen with new layout
    Refactored HomeArtists to HomeScreen with state management and added UI components for playlists and artists.

--- lib/src/features/home/view/home_artists.dart
+++ lib/src/features/home/view/home_artists.dart
@@ -1,56 +1,146 @@
-// Copyright 2022 The Flutter Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
 
 import '../../../shared/classes/classes.dart';
 import '../../../shared/extensions.dart';
+import '../../../shared/providers/providers.dart';
+import '../../../shared/views/views.dart';
+import '../../../utils/adaptive_components.dart';
+import '../../playlists/view/playlist_songs.dart';
+import 'view.dart';
 
-class HomeArtists extends StatelessWidget {
-  const HomeArtists({
-    super.key,
-    required this.artists,
-    required this.constraints,
-  });
-
-  final List<Artist> artists;
-  final BoxConstraints constraints;
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(15),
-      child: constraints.isMobile
-          ? Column(
+    final PlaylistsProvider playlistProvider = PlaylistsProvider();
+    final List<Playlist> playlists = playlistProvider.playlists;
+    final Playlist topSongs = playlistProvider.topSongs;
+    final Playlist newReleases = playlistProvider.newReleases;
+    final ArtistsProvider artistsProvider = ArtistsProvider();
+    final List<Artist> artists = artistsProvider.artists;
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        return Scaffold(
+          body: SingleChildScrollView(
+            child: AdaptiveColumn(
               children: [
-                for (final artist in artists) buildTile(context, artist),
-              ],
-            )
-          : Row(
-              children: [
-                for (final artist in artists)
-                  Flexible(flex: 1, child: buildTile(context, artist)),
+                AdaptiveContainer(
+                  columnSpan: 12,
+                  child: Padding(
+                    padding: const EdgeInsets.all(2),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Expanded(
+                          child: Text(
+                            'Good morning',
+                            style: context.displaySmall,
+                          ),
+                        ),
+                        const SizedBox(width: 20),
+                        const BrightnessToggle(),
+                      ],
+                    ),
+                  ),
+                ),
+                AdaptiveContainer(
+                  columnSpan: 12,
+                  child: Column(
+                    children: [
+                      const HomeHighlight(),
+                      LayoutBuilder(
+                        builder: (context, constraints) => HomeArtists(
+                          artists: artists,
+                          constraints: constraints,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                AdaptiveContainer(
+                  columnSpan: 12,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(2),
+                        child: Text(
+                          'Recently played',
+                          style: context.headlineSmall,
+                        ),
+                      ),
+                      HomeRecent(playlists: playlists),
+                    ],
+                  ),
+                ),
+                AdaptiveContainer(
+                  columnSpan: 12,
+                  child: Padding(
+                    padding: const EdgeInsets.all(2),
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Flexible(
+                          flex: 10,
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Padding(
+                                padding: const EdgeInsets.all(2),
+                                child: Text(
+                                  'Top Songs Today',
+                                  style: context.titleLarge,
+                                ),
+                              ),
+                              LayoutBuilder(
+                                builder: (context, constraints) =>
+                                    PlaylistSongs(
+                                      playlist: topSongs,
+                                      constraints: constraints,
+                                    ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        Flexible(
+                          flex: 10,
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Padding(
+                                padding: const EdgeInsets.all(2),
+                                child: Text(
+                                  'New Releases',
+                                  style: context.titleLarge,
+                                ),
+                              ),
+                              LayoutBuilder(
+                                builder: (context, constraints) =>
+                                    PlaylistSongs(
+                                      playlist: newReleases,
+                                      constraints: constraints,
+                                    ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
               ],
             ),
-    );
-  }
-
-  Widget buildTile(BuildContext context, Artist artist) {
-    return ListTile(
-      leading: CircleAvatar(backgroundImage: AssetImage(artist.image.image)),
-      title: Text(
-        artist.updates.first,
-        maxLines: 2,
-        style: context.labelLarge,
-        overflow: TextOverflow.ellipsis,
-      ),
-      subtitle: Padding(
-        padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 0),
-        child: Text(artist.name, style: context.labelMedium),
-      ),
-      onTap: () => GoRouter.of(context).go('/artists/${artist.id}'),
+          ),
+        );
+      },
     );
   }
 }
